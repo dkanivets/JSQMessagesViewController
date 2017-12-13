2abfdb270852e84ca7e4734bac83edb063ba0e82
Admin <dmitry.kanivets@uvoteam.com>
2017-12-13 14:23:06 +0200

    - removed animations

--- JSQMessagesViewController/Controllers/JSQMessagesViewController.m
+++ JSQMessagesViewController/Controllers/JSQMessagesViewController.m
@@ -805,15 +805,15 @@ static void JSQInstallWorkaroundForSheetPresentationIssue26295020(void) {
         return;
     }
     if (!self.inputToolbar.contentView.textView.hasText) {
-        [UIView animateWithDuration:0.25 animations:^{
+//        [UIView animateWithDuration:0.25 animations:^{
             textView.frame = self.untouchedRect;
-        }];
+//        }];
     } else if (textView.frame.size.width != self.untouchedRect.size.width + 30){
         CGRect rect = textView.frame;
         CGRect newRect = CGRectMake(8, rect.origin.y, rect.size.width + 30, rect.size.height);
-        [UIView animateWithDuration:0.25 animations:^{
+//        [UIView animateWithDuration:0.25 animations:^{
             textView.frame = newRect;
-        }];
+        //        }];:
     }
 }
 
